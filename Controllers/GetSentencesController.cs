c0924be3bbf32b05c592d527a651101826d5a142
kamjom <kamjom.bebong@gmail.com>
2021-11-10 18:57:15 +0200

    added the get sentence controller

---
+++ Controllers/GetSentencesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace Backend2.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+  
+    public class GetSentencesController : Controller
+    {
+        private readonly IConfiguration _config;
+      
+        public GetSentencesController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string Index()
+        {
+            string sentences = "";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(_config["RunninHillDBConnect"]))
+                {
+                    using (SqlCommand command = new SqlCommand(_config["getSentencesQuery"], connection))
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+
+                            while (reader.Read())
+                            {
+                                if (sentences == "")
+                                {
+                                    sentences += reader.GetString(0);
+                                }
+                                else
+                                {
+                                    sentences += "," + reader.GetString(0);
+                                }
+                                
+                          
+                            }
+                        }
+                    }
+
+                }
+                return sentences;
+
+            }
+            catch (SqlException e)
+            {
+                return sentences;
+            }
+        }
+
+    }
+}
